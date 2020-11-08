df8abe4142351fccc7070a120b0dc026c7b6e49c
Fitgerald Afful <fitzafful@gmail.com>
2020-11-08 22:54:01 +0000

    Created API methods for GET and POST articles

---
+++ app/controllers/api/v1/articles_controller.rb
@@ -0,0 +1,34 @@
+module Api
+    module V1
+        class ArticlesController < ApplicationController
+            skip_before_action :verify_authenticity_token
+            def index
+                articles = Article.order('created_at DESC')
+                render json: {status: 'success', message: 'articles loaded successfully', data: articles}, status: :ok
+            end
+
+            def show
+                article = Article.find(params[:id])
+                render json: {status: 'success', message: 'article loaded successfully', data: article}, status: :ok
+            end
+
+            def create
+                article = Article.new(article_params)
+
+                if article.save
+                    puts "A string".inspect
+                    render json: {status: 'success', message: 'article saved successfully', data: article}, status: :ok
+                else 
+                    puts "Error string".inspect
+                    render json: {status: 'error', message: 'article not saved', data: article.errors}, status: :unprocessable_entity
+                end
+            end
+
+            private
+
+            def article_params
+                params.permit(:title, :body)
+            end
+        end
+    end
+end
